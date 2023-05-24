ed5fcd12f8847620337130de2b7962d07dcbee1e
Mykyta Abramenko <mykyta.abramenko@plus4u.net>
2023-05-25 00:48:04 +0300

    Completed WebAPI task

---
+++ spec/test-constants.ts
@@ -0,0 +1,8 @@
+// add valid token before running tests
+export const accessToken =
+  "sl.BfB6P1_Isa3-tJeSAGot9QvJLGTHIu9g3NTLAiIaL30rOlEBamlH_1bw8lGq71s1Ba18quFQcEQdqePFo6-gmhk9SxSLFYMSy9QZGthnadF8lEsbJrJTB8wSTXR_Au2VKat6w3SsFF0Y";
+export const expiredAccessToken =
+  "sl.BfCUztDvWLPNJo45rkVgOYl2iqlBDVL31B1T0NJKosj9gwd11_QQV7dusHqB0XvxOtZXFGJTbUHVjaxu1uGkrvDyCcSZWY7pqWnP5N54qBx0-nS51ZtLfI9ccqpC4iTEDrnq94s";
+export const dropboxPath = "/test.txt";
+export const incorrectDropboxPath = "/incorrect-test.txt";
+export const fileName = "test.txt";
